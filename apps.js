ed0f319af186366ce79545f52d16e3532f0486f3
Brionna White <Brionnanwhite@gmail.com>
2016-08-19 09:40:24 -0700

    commit

--- apps.js
+++ apps.js
@@ -1,16 +1,23 @@
 var Shoes = (function () {
     function Shoes() {
-        this.shoeName = 'tennis shoes';
-        this.price = 69.99;
     }
     return Shoes;
 }());
-var shoe1 = new Shoes();
-var shoeItem = document.getElementById("shopping-cart");
-shoeItem.innerHTML = shoe1.shoeName + " -$ " + shoe1.price;
-var shoe2 = new Shoes();
-shoe2.shoeName = 'high heel';
-shoe2.price = 49.99;
-shoeItem.innerHTML = shoe2.shoeName + " -$ " + shoe2.price;
-console.log(shoe2.shoeName);
-console.log(shoe1.shoeName);
+var nike = new Shoes();
+nike.shoeName = 'airmax';
+nike.price = 89.99;
+var addidas = new Shoes();
+addidas.shoeName = 'classic';
+addidas.price = 99.99;
+var jordans = new Shoes();
+jordans.shoeName = '12s';
+jordans.price = 159.99;
+function itClicks() {
+    console.log('itClicks');
+}
+var shoe1 = document.getElementById('output');
+shoe1.innerHTML = nike.shoeName + " -$" + nike.price;
+var shoe2 = document.getElementById('output2');
+shoe2.innerHTML = addidas.shoeName + " -$" + addidas.price;
+var shoe3 = document.getElementById('output3');
+shoe3.innerHTML = jordans.shoeName + " -$" + jordans.price;
--- apps.ts
+++ apps.ts
@@ -1,25 +1,37 @@
 class Shoes {
       shoeName:string
       price:number
-    constructor() {
-      this.shoeName = 'tennis shoes'
-      this.price = 69.99
-  
+      constructor() {
+
     }
-
 }
-let shoe1 = new Shoes ();
-let shoeItem = <HTMLElement>document.getElementById("shopping-cart");
-shoeItem.innerHTML = `${shoe1.shoeName} -$ ${shoe1.price}`;
+let nike = new Shoes ()
+nike.shoeName = 'airmax'
+nike.price = 89.99
 
-let shoe2 = new Shoes();
- shoe2.shoeName= 'high heel';
- shoe2.price = 49.99;
- shoeItem.innerHTML = `${shoe2.shoeName} -$ ${shoe2.price}`;
+let addidas = new Shoes ()
+addidas.shoeName = 'classic'
+addidas.price = 99.99
+
+let jordans = new Shoes ()
+jordans.shoeName = '12s'
+jordans.price = 159.99
+
+function itClicks(){
+  console.log('itClicks')
+}
 
 
-console.log(shoe2.shoeName)
-console.log(shoe1.shoeName)
+
+let shoe1 = <HTMLElement>document.getElementById('output')
+ shoe1.innerHTML = `${nike.shoeName} -$${nike.price}`;
+
+ let shoe2 = <HTMLElement>document.getElementById('output2')
+  shoe2.innerHTML = `${addidas.shoeName} -$${addidas.price}`;
+
+let shoe3 = <HTMLElement>document.getElementById('output3')
+   shoe3.innerHTML = `${jordans.shoeName} -$${jordans.price}`;
+
 
 // add onclick
 // add cart section
